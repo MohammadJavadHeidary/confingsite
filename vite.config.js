c5857a2b031394938ef2d01e3f20aa3c339db712
Javad-heidary <dev.javad.sh.498@gmail.com>
2026-05-04 16:31:58 +0330

    delete pwa

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Todo App',
-        short_name: 'Todo',
-        theme_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
-  base: "/"
+  plugins: [react(),tailwindcss()],
 })
